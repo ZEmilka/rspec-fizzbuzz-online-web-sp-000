0250088e97eef5a0c77ce197176976e7e8483813
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-30 17:26:14 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -31,16 +31,16 @@ end
 #  puts"FizzBuzz"
 #end
 #1.upto(100) do |number|
-  if number % 3 == 0 && number % 5 == 0
-    puts 'FizzBuzz'
-  elsif number % 3 == 0
-    puts 'Fizz'
-  elsif number % 5 == 0
-    puts 'Buzz'
-  else
-    puts number
-  end
-end
+#  if number % 3 == 0 && number % 5 == 0
+#    puts 'FizzBuzz'
+#  elsif number % 3 == 0
+#    puts 'Fizz'
+#  elsif number % 5 == 0
+#    puts 'Buzz'
+#  else
+#    puts number
+#  end
+#end
 def fizzbuzz(3)
   1.upto(100) do |fizz_3|
     puts"Fizz"
