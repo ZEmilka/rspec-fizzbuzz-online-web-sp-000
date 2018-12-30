dec25fefaef640427a1c38adcacbb75f32590b80
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-30 17:08:21 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -30,14 +30,14 @@ end
 #elsif numbers/15
 #  puts"FizzBuzz"
 #end
-1.upto(100) do |n|
-  if n % 3 == 0 && n % 5 == 0
+1.upto(100) do |number|
+  if number % 3 == 0 && number % 5 == 0
     puts 'FizzBuzz'
-  elsif n % 3 == 0
+  elsif number % 3 == 0
     puts 'Fizz'
-  elsif n % 5 == 0
+  elsif number % 5 == 0
     puts 'Buzz'
   else
-    puts n
+    puts number
   end
 end
