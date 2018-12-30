55309e3d1748fd5413545b5e3f4651e73a9c9ba9
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-30 17:11:55 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -31,11 +31,11 @@ end
 #  puts"FizzBuzz"
 #end
 1.upto(100) do |number|
-  if number / 3 == 0 && number / 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     puts 'FizzBuzz'
-  elsif number / 3 == 0
+  elsif number % 3 == 0
     puts 'Fizz'
-  elsif number / 5 == 0
+  elsif number % 5 == 0
     puts 'Buzz'
   else
     puts number
