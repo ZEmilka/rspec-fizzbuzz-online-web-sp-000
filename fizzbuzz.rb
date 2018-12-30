29042d95b6a0c496e800bdd459eb08785301c822
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-30 17:56:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,15 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
+    puts 'The number is #{number}, and we returned FizzBuzz'
     return 'FizzBuzz'
   elsif number % 3 == 0
-puts number
-puts"Fizz"
+    puts 'The number is #{number}, and we returned Fizz'
     return 'Fizz'
   elsif number % 5 == 0
+    puts 'The number is #{number}, and we returned Buzz'
     return 'Buzz'
   else
+    puts 'The number is #{number}, and we returned nil'
     return nil
   end
 end
